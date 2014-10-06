5c5f2593a37090a958e9625fec95a9e25897e995
divyanarasimhan <npdv2007@gmail.com>
2014-10-06 13:29:31 +0530

    [#sani2]test from git

--- skydeskprojects_test.rb
+++ skydeskprojects_test.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../helper', __FILE__)
 
-class SkyDeskProjectsTest < Service::TestCase
-  
+class SkyDeskProjectsTest < Service::TestCase[]
   def setup
     @stubs = Faraday::Adapter::Test::Stubs.new
   end
